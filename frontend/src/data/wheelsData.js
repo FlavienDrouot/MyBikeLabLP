a1b088396dd4ee4f7d025fd4e05351ccfe0a1f49
Flavien Drouot <flavien.drouot@gmail.com>
2026-06-04 10:57:33 +0200

    Data : Caden + PandaPodium

--- frontend/src/data/wheelsData.js
+++ frontend/src/data/wheelsData.js
@@ -2,10 +2,12 @@ import { mavicWheels } from './wheelsData_mavic';
 import { rovalWheels } from './wheelsData_roval';
 import { zippWheels } from './wheelsData_zipp';
 import { enveWheels } from './wheelsData_enve';
+import { cadenWheels } from './wheelsData_caden';
 
 export const wheelsData = [
   ...mavicWheels,
   ...rovalWheels,
   ...zippWheels,
   ...enveWheels,
+  ...cadenWheels,
 ];
